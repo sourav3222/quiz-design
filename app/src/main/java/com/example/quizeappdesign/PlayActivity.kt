73443433d0b13865d5077b5b1290ade7d47082e7
sourav <ramsourav82@gmail.com>
2024-09-02 17:27:25 +0600

    design1

--- app/src/main/java/com/example/quizeappdesign/PlayActivity.kt
+++ app/src/main/java/com/example/quizeappdesign/PlayActivity.kt
@@ -1,10 +1,7 @@
 package com.example.quizeappdesign
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.quizeappdesign.databinding.ActivityPlayBinding
 
 class PlayActivity : AppCompatActivity() {
@@ -12,17 +9,33 @@ class PlayActivity : AppCompatActivity() {
 
     val quizList = listOf<Quiz>(
         Quiz(
-            "victory day of bangladesh"
-            "12 jan"
-            "13 jan"
-            "16 dec"
-        )
+            "victory day of bangladesh?",
+            "16 dec",
+            "16 aug",
+            "13 aug",
+            "12 aug" ,
+            "16 dec")
     )
+    var  index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlayBinding.inflate(layoutInflater)
         setContentView(binding.root)
+         initquestion()
+    }
+
+    fun initquestion() {
+
+        val quiz = quizList[index]
+
+        binding.apply {
+            questionTV.text = quiz.question
+            option1.text = quiz.option1
+            option2.text = quiz.option2
+            option3.text = quiz.option3
+            option4.text = quiz.option4
+        }
 
     }
 }
--- app/src/main/java/com/example/quizeappdesign/Quiz.kt
+++ app/src/main/java/com/example/quizeappdesign/Quiz.kt
@@ -1,10 +1,10 @@
 package com.example.quizeappdesign
 
 data class Quiz(
-    val question : String,
-    val option1 : String,
-    val option2 : String,
-    val option3 : String,
-    val option4 : String,
-    val Answer : String
+    val question: String,
+    val option1: String,
+    val option2: String,
+    val option3: String,
+    val option4: String,
+    val Answer :String
 )
