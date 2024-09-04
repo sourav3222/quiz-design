d7e95b18488b707c249fc4b81308b5ea8b96c7a4
App <ramsourav82@gmail.com>
2024-09-04 14:02:52 +0600

    quiz compleate

--- app/src/main/java/com/example/quizeappdesign/ResultActivity.kt
+++ app/src/main/java/com/example/quizeappdesign/ResultActivity.kt
@@ -5,14 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quizeappdesign.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    lateinit var binding: ActivityResultBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_result)
+        binding= ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+      val skip =   intent.getIntExtra("skip",0)
+       val carrect =  intent.getIntExtra("carrect",0)
+       val worng =  intent.getIntExtra("worng",0)
 
 
+        binding.resltTv.text = "skip : $skip\n"+ "carrect: $carrect\n"+"worng: $worng"
     }
 }
